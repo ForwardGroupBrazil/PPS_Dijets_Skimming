7d3708aa6784d6cadeb06367ca7016bb6559e63c
Diego Figueiredo <dmf@cern.ch>
2014-05-05 16:36:00 +0200

    new TTree and PPS Objects.

--- ExclusiveDijetsAnalysisUsingPPS/test/DijetsAnalyzer_cfg.py
+++ ExclusiveDijetsAnalysisUsingPPS/test/DijetsAnalyzer_cfg.py
@@ -7,27 +7,30 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
-    # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
-#        'file:/storage2/sfonseca/CMSSW/PPS_Analysis/CMSSW_6_2_0/src/PPS_Dijets_Skimming/patTuple_PF2PAT.root'
-         'file:/afs/cern.ch/work/p/polme/public/PPS/CMSSW_6_2_0/src/test_GG_exhume.root'
+          'file:/afs/cern.ch/work/p/polme/public/PPS/CMSSW_6_2_0/src/GG_newtest_1.root'
     )
 )
 
+process.ak5JetTracksAssociatorAtVertex = cms.EDProducer("JetTracksAssociatorAtVertex",
+      	                                                 tracks = cms.InputTag("generalTracks"),
+                                                         jets   = cms.InputTag("ak5PFJets"),
+                                                         coneSize = cms.double(0.5)
+                                                       )
+
+
 process.demo = cms.EDAnalyzer('ExclusiveDijetsAnalysisUsingPPS',
-			       ## jet collections ###########################
 			       JetTag = cms.InputTag("ak5PFJets"),
 			       ParticleFlowTag = cms.InputTag("particleFlow"),
-			       PPSTag = cms.untracked.string("PPSReco")			
+			       PPSTag = cms.untracked.string("PPSReco"),
+                               pTPFThresholdCharged = cms.double(0.1),
+                               energyPFThresholdBar = cms.double(1.5),
+                               energyPFThresholdEnd = cms.double(3.5),
+                               energyPFThresholdHF = cms.double(4.0)
                                )
-
-
-
 
 process.TFileService = cms.Service("TFileService",
                                        fileName = cms.string('histo_dijets.root')
                                    )
 
-
-
-process.p = cms.Path(process.demo)
+process.p = cms.Path(process.ak5JetTracksAssociatorAtVertex*process.demo)
